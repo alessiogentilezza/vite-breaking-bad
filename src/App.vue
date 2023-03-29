294616ed08b4f631e486074e8ac6dcf657381026
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-29 20:56:45 +0200

    inserito ritardo 5 secondi

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,15 @@ import { store } from './store.js';
 import TheHeader from './components/TheHeader.vue';
 import AppSearch from './components/AppSearch.vue';
 import CharactersList from './components/CharactersList.vue';
+import Loading from './components/Loading.vue';
+
 
 export default {
   components: {
     TheHeader,
     AppSearch,
     CharactersList,
+    Loading,
   },
   data() {
     return {
@@ -20,13 +23,23 @@ export default {
   methods: {
     getCharacters() {
       axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?cardset=metal%20raiders&num=18&offset=0')
-      // axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Blue-Eyes')
+        // axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Blue-Eyes')
 
-      
-      .then(response => {
-        this.store.charactersList = response.data.data;
-        console.log(this.store.charactersList)
-      });
+
+        .then(response => {
+          this.store.loading = true;
+
+          setTimeout(() => {
+            this.store.loading = false;
+
+            console.log("this is the first message");
+          }, 5000);
+
+
+          this.store.charactersList = response.data.data;
+          console.log(this.store.charactersList)
+
+        });
     }
   },
   created() {
@@ -39,11 +52,12 @@ export default {
 <template>
   <TheHeader></TheHeader>
   <main>
-    <AppSearch/>
+    <AppSearch />
     <CharactersList />
 
   </main>
 
+  <Loading />
 </template>
 
 <style lang="scss">
