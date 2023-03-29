b4c2090ea30d094f0433c107c8a3dbcac7caa174
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-29 14:56:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script >
+import TheHeader from "./components/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+
+</script>
+
+<template>
+  
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+
+</div>
+
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "TheHeader",
+
+  data() {
+    return {
+    };
+  },
+
+};
+</script>
+
+<template></template>
+
+<style scoped lang="scss">
+@use "../styles/partials/variables" as *;
+</style>
